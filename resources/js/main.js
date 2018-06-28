6d2a6ff8144fea853f04c281747993f824ac5200
Kim Yoon Sung <yoonsung0711@gmail.com>
2018-06-29 05:22:42 +0900

    (U)pdate resources/js/main.js : add validation check for input box

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,7 @@
-function buttonClick() {
-    console.log('Button Clicked!');
-}
-document.getElementById('add').addEventListener('click', buttonClick );
+
+document.getElementById('add').addEventListener('click', function(){
+    var value = document.getElementById('item').value;
+    if (value) {
+        console.log('there was a value, wooh!');
+    }
+ });
